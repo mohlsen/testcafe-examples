ad5dee4bd2350ef68c36bcfb38b0c2237da347c8
Mike Ohlsen <mjohlsen@ra.rockwell.com>
2021-03-30 10:44:24 -0400

    added more documentation and code examples

--- examples/unhandled-exception-promise/README.md
+++ examples/unhandled-exception-promise/README.md
@@ -1,1 +1,20 @@
-run `npx testcafe chrome examples/unhandled-exception-promise`
+# Testcafe timeout issue
+
+There seems to be a condition when code under test throws an unhandled exception or if a asyn function never completes, testcafe will just hang forever, 
+and does not respect any timeout flags.
+
+For this example test, run the following:
+`npx testcafe chrome examples/unhandled-exception-promise`
+
+and to be specific with timeouts try:
+`npx testcafe chrome examples/unhandled-exception-promise --selector-timeout 10000 --assertion-timeout 10000 --page-load-timeout 10000 --ajax-request-timeout 10000 --page-request-timeout 10000 --browser-init-timeout 10000`
+
+The test will just run forever.
+
+## Code
+
+The code in `unhandled.js` will hang forever as is. There are several other examples commented out in the code that also show the same problem.  We realize 
+that code under test (or in setup) that never resolves, rejects or throws is bad.  But it happens in real world code that is hard to prevent. We are 
+questioning why testcafe never times out in these condition, it will run forever, not respecting any timeout flags. In this case, we need to rely on our
+CI infrastructure (jenkins)to timeout). While this works, it is configured much longer since it encapsulated the entire test and CI setup, and it should
+rather handle test timeouts with testcafe so it can be controled per test.
--- examples/unhandled-exception-promise/unhandled.js
+++ examples/unhandled-exception-promise/unhandled.js
@@ -1,8 +1,12 @@
 import { Selector } from 'testcafe';
 
 process.on('uncaughtException', (err, origin) => {
-    console.log('uncaughtException', {err, origin});
-    //process.exit(1);
+    console.log('uncaughtException, this will cause testcafe to wait forever', {err, origin});
+    // I would have assumed that if this is hit, testcafe would fail because we did not use --skip-uncaught-errors,
+    // but it does not fail.
+
+    // this will kill the test. 
+    // process.exit(1);
 });
 
 fixture `Page manipulation`
@@ -11,50 +15,53 @@ fixture `Page manipulation`
 
         console.log('before');
         
-        return await new Promise((resolve, reject ) => {
+        await new Promise((resolve, reject ) => {
 
-            setTimeout(function() {
-                console.log('before throw');
-                throw new Error('Throwing!');
-                console.log('after throw');
-                resolve();
-            }, 1000);
+            // with nothing in this function body (never resolving or rejecting), testcafe will wait forever, and never timeout
 
-            //throw new Error('Throwing!');
+            // The following line will cause the async function to reject, and the test will fail appropriately.
+            // throw new Error('Throwing!');
 
-            //reject('reject');
-          });
+            // The following line will cause the promise to reject, this cause the async function to reject, and the test will fail appropriately.
+            // reject('reject');
+
+            // The following line will never resolve or reject the promise. the exception will be seen as unhandled by node.js.
+            // setTimeout(function() {
+            //     console.log('before throw');
+            //     throw new Error('Throwing!');
+            // }, 1000);
+
+        });
         
     })
     .beforeEach(async () => {
         console.log('before each');
 
+        // with nothing in this function body (never resolving or rejecting), testcafe will wait forever, and never timeout (just like in the before hook)
+        // await new Promise((resolve, reject ) => { });
+
     })
     .afterEach(async () => {
         console.log('after each');
 
+        // with nothing in this function body (never resolving or rejecting), testcafe will wait forever, and never timeout (just like in the before hook)
+        // await new Promise((resolve, reject ) => { });
     })
     .after(async () => {
         console.log('after');
+
+        // with nothing in this function body (never resolving or rejecting), testcafe will wait forever, and never timeout (just like in the before hook)
+        // await new Promise((resolve, reject ) => { });
     });
 
     
-// async function throwingFunction() {
-//     return await new Promise((resolve) => {
-//         setTimeout(function() {
-//             console.log('before throw');
-//             throw new Error('Throwing!');
-//             console.log('after throw');
-//             resolve();
-//         }, 1000);
-//       });
-// }
 
 test('Fill a textbox', async t => {
 
-    // await throwingFunction().catch((err) => {
-    //     console.log('catch', {err});
-    // }) 
+    // adding the following will cause the test to run forever, it will not timeout. (just like in the other hooks)
+    // await new Promise((resolve, reject ) => {
+
+    // });
 
     await t.typeText('#developer-name', 'Peter Parker');
 
